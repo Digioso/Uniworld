2081761a7d6b3b247031a3d5b0d642e6f0157b82
Digioso <Digioso@users.noreply.github.com>
2025-07-16 13:40:03 +0200

    Fixed install.bat file
    The install batch only executed the first command.

--- install.bat
+++ install.bat
@@ -1,8 +1,8 @@
-cpan CPAN
-cpan PDF::API2
-cpan Browser::Open
+call cpan CPAN
+call cpan PDF::API2
+call cpan Browser::Open
 set LC_ALL=C
 set LANG=C
-cpanm https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/patched_cpan_modules/Tk-804.036_001.tar.gz
-cpan Tk::Balloon
+call cpanm https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/patched_cpan_modules/Tk-804.036_001.tar.gz
+call cpan Tk::Balloon
 pause
