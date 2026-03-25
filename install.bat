b2da0a6d2c7a1416d7f401b22dacf7a0de443907
Digioso <Digioso@users.noreply.github.com>
2026-03-25 16:10:46 +0100

    Delete install.bat
    funktioniert nicht als batch datei

--- install.bat
+++
@@ -1,8 +0,0 @@
-call cpan CPAN
-call cpan PDF::API2
-call cpan Browser::Open
-set LC_ALL=C
-set LANG=C
-call cpanm https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/patched_cpan_modules/Tk-804.036_001.tar.gz
-call cpan Tk::Balloon
-pause
